d4652e49ff39af6ba8a4dfdbfcd6e1678786349c
Paul June Domag <paul.do@user.com.sg>
2023-07-07 01:22:19 +0800

    :art: Folder restructure
    :zap: Converted main call to async

---
+++ Services/ClassifierExtension.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Language.StandardClassification;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using System.Threading.Tasks;
+
+namespace Fontify.Services
+{
+    internal class ClassifierExtension
+    {
+        private static readonly AsyncLazy<ClassifierExtension> instance;
+        static ClassifierExtension() => instance = new AsyncLazy<ClassifierExtension>(CreateInstanceAsync, ThreadHelper.JoinableTaskFactory);
+        private static async Task<ClassifierExtension> CreateInstanceAsync() => await Task.FromResult(new ClassifierExtension());
+        public static async Task<ClassifierExtension> GetInstanceAsync() => await instance.GetValueAsync();
+        public static ClassifierExtension Instance => instance.GetValue();
+
+        private bool IsExecuting = false;
+        private Typeface normalTypeface;
+        private Typeface boldTypeface;
+        private Typeface italicTypeface;
+        private Typeface bolditalicTypeface;
+        private string[] italicClassifiers;
+        private ClassifierExtension()
+        {
+            normalTypeface = new Typeface("JetBrains Mono Regular");
+            boldTypeface = new Typeface("JetBrains Mono ExtraBold");
+            italicTypeface = new Typeface("JetBrains Mono ExtraLight Italic");
+            bolditalicTypeface = new Typeface("JetBrains Mono Italic");
+            italicClassifiers = new string[] {
+                PredefinedClassificationTypeNames.Comment,
+                PredefinedClassificationTypeNames.String,
+                "interface name",
+                "keyword - control",
+                "method name",
+                "namespace name"
+            };
+        }
+
+        public async Task OverrideFormatMapAsync(IClassificationTypeRegistryService ctrs, IClassificationFormatMapService cfms)
+        {
+            if (!IsExecuting)
+            {
+                try
+                {
+                    IsExecuting = true;
+
+                    var cfm = cfms.GetClassificationFormatMap(category: "text");
+
+                    var defaultProps = cfm.DefaultTextProperties
+                        .SetTypeface(normalTypeface);
+                    cfm.DefaultTextProperties = defaultProps;
+
+                    var propertyUpdates = await UpdatePropertiesAsync(cfm);
+
+                    cfm.BeginBatchUpdate();
+                    propertyUpdates.ForEach(item => cfm.SetExplicitTextProperties(item.ct, item.props));
+                    cfm.EndBatchUpdate();
+                }
+                catch (Exception ex)
+                {
+                    ErrorHandler.ExceptionToHResult(new Exception(
+                        message: $"[Fontify] Exception occured on {ex.Source} with message {ex.Message}",
+                        innerException: ex
+                    ));
+                } finally
+                {
+                    IsExecuting = false;
+                }
+            }
+        }
+
+        private async Task<List<(IClassificationType ct, TextFormattingRunProperties props)>> UpdatePropertiesAsync(IClassificationFormatMap cfm)
+        {
+            var updatedItems = new List<(IClassificationType ct, TextFormattingRunProperties props)>();
+            var classifiers = cfm.CurrentPriorityOrder
+                .Where(x => x != default)
+                .Select(x => new {
+                    Name = x.Classification,
+                    Properties = cfm.GetExplicitTextProperties(x),
+                    ClassificationType = x })
+                .Where(x => x.Properties.Bold || x.Properties.Italic || italicClassifiers.Contains(x.Name));
+
+            foreach (var item in classifiers)
+            {
+                try
+                {
+                    var isItalic = italicClassifiers.Contains(item.Name);
+                    var isBold = item.Properties.Bold;
+                    var isBoldItalic = isBold && isItalic;
+                    var typeface =
+                            isBoldItalic ? bolditalicTypeface :
+                            isBold ? boldTypeface :
+                            isItalic ? italicTypeface :
+                            default;
+
+                    if (typeface != default)
+                    {
+                        updatedItems.Add((item.ClassificationType, item.Properties.SetTypeface(typeface)));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(
+                        message: $"Unable to apply font to ClassifierType: {item.Name} due to an error.",
+                        innerException: ex
+                    );
+                }
+            }
+
+            return await Task.FromResult(updatedItems);
+        }
+    }
+}
